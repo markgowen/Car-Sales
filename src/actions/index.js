e59cecaaea1e18009f1a9dbdf7838dc65761ef61
Mark Gowen <mark.gowen78@gmail.com>
2019-10-09 15:47:41 -0700

    PR
    created reducer and actions,
    button to add not working
    
    not completed

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+export const ADD_FEATURE = 'ADD_FEATURE';
+export const addFeature = (item) => ({ type: ADD_FEATURE, payload: item});
---
+++ src/reducers/index.js
@@ -0,0 +1,50 @@
+import { ADD_FEATURE } from '../actions'
+
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_FEATURE:
+          const addedFeature = state.additionalFeatures.find(item => {
+              return item.id === action.payload
+          })
+          const newState = {
+              ...state,
+              car: {
+                ...state.car,
+                price: state.car.price + addedFeature.price,
+                features: [
+                    ...state.car.features,
+                    addedFeature
+                ],
+              },
+              additionalFeatures: state.additionalFeatures.filter(item => {
+                  if (item.id === action.payload) {
+                      return false;
+                  } else {
+                      return true;
+                  }
+              })
+          }
+            return newState;
+        
+        default:
+            return state;
+    }
+}
+
